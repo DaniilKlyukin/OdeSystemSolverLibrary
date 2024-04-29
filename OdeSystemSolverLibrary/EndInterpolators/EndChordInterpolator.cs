f5af46ef06fba7ec8c4d6a689d4398f4bec70161
Daniil_Klyukin <anatoliikljukin@mail.ru>
2024-04-29 16:37:24 +0400

    Update EndChordInterpolator.cs

--- OdeSystemSolverLibrary/EndInterpolators/EndChordInterpolator.cs
+++ OdeSystemSolverLibrary/EndInterpolators/EndChordInterpolator.cs
@@ -11,37 +11,29 @@
 
         public override (double, double[]) Interpolate(OdeStepSolver solver, double t0, double[] x0, double t1, double[] x1)
         {
+            solver.t = t1;
+            Array.Copy(x1, solver.x, x1.Length);
+
             double f_0 = OdeDistanceToStop.Invoke(t0, x0);
             double f_1 = OdeDistanceToStop.Invoke(t1, x1);
 
             while (true)
             {
                 var tTemp = t0 - f_0 * (t1 - t0) / (f_1 - f_0);
-
-                var xTemp = new double[x1.Length];
-                for (int j = 0; j < x1.Length; ++j)
-                    xTemp[j] = x0[j] - f_0 * (x1[j] - x0[j]) / (f_1 - f_0);
-
                 double dt = tTemp - t1;
 
-                solver.t = t1;
                 solver.dt = dt;
-                Array.Copy(x1, solver.x, x1.Length);
                 solver.SolveStep();
 
                 var fTemp = OdeDistanceToStop?.Invoke(solver.t, solver.x) ?? 0;
 
-                if (fTemp > 0)
-                {
-                    t1 = solver.t;
-                    Array.Copy(solver.x, x1, x1.Length);
-                }
-                else if (fTemp < 0)
-                {
-                    t0 = solver.t;
-                    Array.Copy(solver.x, x1, x1.Length);
-                }
-                else if (Math.Abs(fTemp) <= tolerance)
+                t0 = t1;
+                t1 = tTemp;
+
+                f_0 = f_1;
+                f_1 = fTemp;
+
+                if (Math.Abs(fTemp) <= tolerance || Math.Abs(dt) <= tolerance)
                     return (solver.t, solver.x);
             }
         }
